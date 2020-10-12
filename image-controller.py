9eb16f7f3d4dd79d32afb25745dae9ed5c034e0b
Henrik Enger Milo <hanas879@milo.life>
2020-10-12 16:33:25 +0200

    made simple menu layout and options

--- image-controller.py
+++ image-controller.py
@@ -1,9 +1,12 @@
 import json
+import os
 import os.path
 import wget
 from bs4 import BeautifulSoup
 import requests
 
+def clear():
+    os.system("cls" if os.name=="nt" else "clear")
 
 def grabIso(url):
     source = requests.get(url).text
@@ -23,11 +26,11 @@ def checkJSON():
             f.write("{}")
 
 
-def saveSources():
+def saveSources(distro, url):
     with open("sources.json") as f:
         sources = json.load(f)
 
-    sources["Distro"] = "PLACEHOLDER"
+    sources[distro] = url
 
     with open("sources.json", "w") as f:
         json.dump(sources, f, indent=2)
@@ -37,3 +40,36 @@ def downloadFile(url):
     print("Downloading: " + url)
     print()
     wget.download(url)
+
+
+def menu():
+    clear()
+    print("What would you like to do?")
+    print()
+    print("1. Add URL to JSON")
+    print("2. Download a specific URL one time")
+    print("3. Exit")
+    print()
+    choice = input()
+    clear()
+    if choice == "1":
+        print("Enter distro NAME:")
+        distroName = input()
+        clear()
+        print("Enter URL:")
+        distroUrl = input()
+        clear()
+        saveSources(distroName,distroUrl)
+        menu()
+    elif choice == "2":
+        clear()
+        downloadFile(input("Enter URL to file:"))
+        menu()
+    elif choice == "3":
+        pass
+    else:
+        menu()
+
+
+checkJSON()
+menu()
