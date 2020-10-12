98111595ec633d5e1e59d7007ebf87d4f9551ae1
Henrik Enger Milo <hanas879@milo.life>
2020-10-12 15:28:19 +0200

    added webscraper to grab .iso link from distrowatch + added requirements

--- image-controller.py
+++ image-controller.py
@@ -1,11 +1,27 @@
 import json
 import os.path
 import wget
+from bs4 import BeautifulSoup
+import requests
+
+
+def grabIso(url):
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, "lxml")
+
+    news = soup.find("td", class_="NewsText")
+    links = news.find_all("a")
+    for i in links:
+        if ".iso" in i["href"] and "64" in i["href"]:
+            print(i["href"])
+            break
+
 
 def checkJSON():
     if os.path.isfile("sources.json") == False:
         with open("sources.json","w") as f:
             f.write("{}")
+
 
 def saveSources():
     with open("sources.json") as f:
@@ -15,6 +31,7 @@ def saveSources():
 
     with open("sources.json", "w") as f:
         json.dump(sources, f, indent=2)
+
 
 def downloadFile(url):
     wget.download(url)
